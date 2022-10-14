5bf79828c162d35c106a7ca79dc6fc88d1279f20
Outi Mustonen <outimus@gmail.com>
2022-10-14 10:32:06 +0300

    4.19

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoose = require('mongoose')
 const { info, error } = require('./utils/logger')
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
